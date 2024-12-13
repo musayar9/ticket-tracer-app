06bb075ea41654c5497f76072d5200ecdecde050
tuncerburak <tuncerburak97@gmail.com>
2024-12-13 22:42:28 +0300

    configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "3xl": "0 10px 24px 0 rgba(68, 60, 98, 0.22)",
+      },
+    },
   },
-  plugins: [],
-}
-
+  daisyui: {
+    themes: ["light"],
+  },
+  plugins: [require("daisyui")],
+};
